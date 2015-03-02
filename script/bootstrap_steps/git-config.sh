7de81c86f2ec85e46d273a95babaa399d0e6d4aa
Enrico Genauck <kontakt@enricogenauck.de>
2015-03-02 09:12:02 +0100

    adds git credentials

---
+++ script/bootstrap_steps/git-config.sh
@@ -0,0 +1,5 @@
+git config --global user.name "Enrico Genauck"
+git config --global user.email "kontakt@enricogenauck.de"
+
+git config --global core.editor "subl -w"
+git config --global color.ui true
