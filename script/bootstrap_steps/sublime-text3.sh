79daf2b2f7039f20909a32074253be30efebb4c0
Enrico Genauck <kontakt@enricogenauck.de>
2015-03-02 07:56:19 +0100

    adds sublime text 3 setup

---
+++ script/bootstrap_steps/sublime-text3.sh
@@ -0,0 +1,10 @@
+brew cask install sublime-text3
+
+DOTFILES=~/.dotfiles/Sublime\ Text\ 3
+CONFIG=~/Library/Application\ Support/Sublime\ Text\ 3
+
+mkdir -p "$CONFIG"
+
+ln -s "$DOTFILES/Installed Packages" "$CONFIG/Installed Packages"
+ln -s "$DOTFILES/Packages" "$CONFIG/Packages"
+ln -s "$DOTFILES/Pristine Packages" "$CONFIG/Pristine Packages"
