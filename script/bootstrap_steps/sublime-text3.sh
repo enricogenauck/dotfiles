50ac86829f5a0eb3011d11d3c54305d3c19298eb
Enrico Genauck <kontakt@enricogenauck.de>
2015-03-02 09:05:05 +0100

    adds sublime ruby markers package and depending gem

--- script/bootstrap_steps/sublime-text3.sh
+++ script/bootstrap_steps/sublime-text3.sh
@@ -9,3 +9,5 @@ mkdir -p "$CONFIG"
 ln -s "$DOTFILES/Installed Packages" "$CONFIG/Installed Packages"
 ln -s "$DOTFILES/Packages" "$CONFIG/Packages"
 ln -s "$DOTFILES/Pristine Packages" "$CONFIG/Pristine Packages"
+
+gem install rcodetools
