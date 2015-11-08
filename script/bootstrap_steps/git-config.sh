8fea70aaad7b6290b6b6712dc14d000559993e8f
Enrico Genauck <kontakt@enricogenauck.de>
2015-11-08 19:11:41 +0100

    Specifies atom as git commit message editor
    Specifies atom as git commit message editor via
    git config. Now atom opens when you git commit.

--- script/bootstrap_steps/git-config.sh
+++ script/bootstrap_steps/git-config.sh
@@ -3,5 +3,5 @@
 git config --global user.name "Enrico Genauck"
 git config --global user.email "kontakt@enricogenauck.de"
 
-git config --global core.editor "subl -w"
+git config --global core.editor "atom --wait"
 git config --global color.ui true
